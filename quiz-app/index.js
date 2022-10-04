f194e19a0064182625ad368df14c5922ddbfbd85
Domains18 <gibsonsgibson88@gmail.com>
2022-10-04 16:41:10 +0300

    Javascript functionality

---
+++ quiz-app/index.js
@@ -0,0 +1,18 @@
+//assigning variables
+let detailForm = document.getElementsByClassName('form-');
+let name = document.getElementById('name');
+let age = document.getElementById('age');
+let categories=document.getElementsByClassName('categories')
+//submit variables
+let submitForm = document.getElementById('submitForm');
+
+//lets build that app ;)
+//set question UI to none
+for(i=0; i<categories.length; i++){
+    categories[i].classList.add('display');
+}
+
+//validate user and age
+submitForm.addEventListener('click', ()=>{
+    if()
+});
